4c03e14afc4c891299d50870d396772a1689c0e6
Huma Muhammad Yousaf <126263235+humabhutti05@users.noreply.github.com>
2025-07-17 19:20:49 +0500

    Changessss

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // ✅ Add this line
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
